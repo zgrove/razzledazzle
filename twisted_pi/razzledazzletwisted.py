b0bfc8c94e3de3af16e34c01877ca1e7f1248a38
Connor Brizendine <cmanvt14@vt.edu>
2014-12-13 17:43:34 -0500

    added the HTML and Twisted server files

---
+++ twisted_pi/razzledazzletwisted.py
@@ -0,0 +1,22 @@
+from twisted.internet import reactor
+from twisted.web.resource import Resource
+from twisted.web.server import Site
+
+import cgi
+import subprocess
+
+class RazzleDazzlePage(Resource):
+    isLeaf = True
+    def render_GET(self, request):
+        return subprocess.check_output("cat razzledazzlepage.html", shell=True);
+
+    def render_POST(self, request):
+        return """
+            <html>
+                <body>You submitted: %s</body>
+            </html>
+        """ % (cgi.escape(request.args["message-field"][0]), )
+        
+factory = Site(RazzleDazzlePage())
+reactor.listenTCP(10000, factory)
+reactor.run()
