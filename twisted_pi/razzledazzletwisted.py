c7c6b91f5dc670a2efed95b800a61416bf6cbef1
Xamists <sasmitman@gmail.com>
2014-12-14 14:55:53 -0500

    Entered all the credentials

--- twisted_pi/razzledazzletwisted.py
+++ twisted_pi/razzledazzletwisted.py
@@ -6,15 +6,23 @@ from twisted.web.server import Site
 
 import cgi
 import subprocess
+import pika
 
 #The IP address
-host = "temphost"
+hostname = "netapps.ece.vt.edu"
 #Virtual host to connec to 
-vhost = "tempvhost"
+vhost = "/2014/fall/archon"
 #User name and password
-credentials = "user:password"
+user = "archon"
+password = Power^Overwhelming96
 #Topic to suscribe to
 routing_key = "messagescroll"
+# Connect to the message broker
+msg_broker = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, virtual_host=vhost, credentials=pika.PlainCredentials(user, password, True)))
+# Setup the exchange
+channel = msg_broker.channel()
+channel.exchange_declare(exchange="razzledazzle_twist", type="direct")
+channel.queue_declare(exclusive=True)
 
 class RazzleDazzlePage(Resource):
 	isLeaf = True
@@ -22,6 +30,7 @@ class RazzleDazzlePage(Resource):
 		return subprocess.check_output("cat razzledazzlepage.html", shell=True);
 
 	def render_POST(self, request):
+		# Set POST message to a variable
 		output=cgi.escape(request.args["message-field"][0])
 		return
 		"""
