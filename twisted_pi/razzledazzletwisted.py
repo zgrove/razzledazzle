43df23169336f85609b9f0c6975d1ae55a4a5383
Xamists <sasmitman@gmail.com>
2014-12-14 14:42:08 -0500

    About to add credentials and ready pushing the post request

--- twisted_pi/razzledazzletwisted.py
+++ twisted_pi/razzledazzletwisted.py
@@ -7,17 +7,29 @@ from twisted.web.server import Site
 import cgi
 import subprocess
 
+#The IP address
+host = "temphost"
+#Virtual host to connec to 
+vhost = "tempvhost"
+#User name and password
+credentials = "user:password"
+#Topic to suscribe to
+routing_key = "messagescroll"
+
 class RazzleDazzlePage(Resource):
 	isLeaf = True
 	def render_GET(self, request):
 		return subprocess.check_output("cat razzledazzlepage.html", shell=True);
 
 	def render_POST(self, request):
-		return """
+		output=cgi.escape(request.args["message-field"][0])
+		return
+		"""
 			<html>
 				<body>You submitted: %s</body>
 			</html>
 		""" % (cgi.escape(request.args["message-field"][0]), )
+		
 
 factory = Site(RazzleDazzlePage())
 reactor.listenTCP(10000, factory)
