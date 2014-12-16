571ad55c61039968439de97ea14b6dafaa221089
CardiganAndTie <aznstriker99@gmail.com>
2014-12-15 23:41:12 -0500

    Message Scrolling via website
    Can receive messages from website and scroll them

--- led_pi/led_subscribe.py
+++ led_pi/led_subscribe.py
@@ -10,14 +10,44 @@ import pika
 
 channel = None
 msg_broker = None
+xmas=['sudo','./led-matrix','-r','16','-D','1','-t','15','Merry_Christmas.ppm']
+newyear=['sudo','./led-matrix','-r','16','-D','1','-t','15','Happy_New_Year.ppm']
+joynloves=['sudo','./led-matrix','-r','16','-D','1','-t','15','Joy_and_Love_to_the_World.ppm']
+hohoho=['sudo','./led-matrix','-r','16','-D','1','-t','15','Ho_Ho_Ho.ppm']
+holiday=['sudo','./led-matrix','-r','16','-D','1','-t','15','Happy_Holidays.ppm']
+
+def print_message(input_message):
+    if input_message == "Merry Christmas!":
+        subprocess.Popen(xmas)
+        time.sleep(16)
+    elif input_message == "Happy New Year!":
+        subprocess.Popen(newyear)
+        time.sleep(16)
+    elif input_message == "Joy and love to the world!":
+        subprocess.Popen(joynloves)
+        time.sleep(16)
+    elif input_message == "Ho Ho Ho!!":
+        subprocess.Popen(hohoho)
+        time.sleep(16)
+    elif input_message == "Happy Holidays!":
+        subprocess.Popen(holiday)
+        time.sleep(16)
+	
 
 # Chat message callback
 def on_chat_msg(channel, method, properties, msg_body):
-	# Get the JSON data as a dictionary
-	data = msg_body
-	# Print out the data to look pretty
-	print data
-	print
+    # Get the JSON data as a dictionary
+    data = msg_body
+    # Print out the data to look pretty
+    print data
+    print_message(data)
+    print
+
+
+
+
+
+
 #The IP address
 hostname = "netapps.ece.vt.edu"
 #Virtual host to connec to 
